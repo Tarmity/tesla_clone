ec37fb23f61ce034a08843fb0922db11576a9fa0
Tarmity Flassman <tarmity.flassman@gmail.com>
2021-06-07 17:38:23 +1000

    done the styling for the hearder using styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
     return (
         <Container>
-            <a href="#"> 
+            <a href="#">
                 <img src="/images/logo.svg" />
             </a>
-        <MenuGroup>
-
-        </MenuGroup>
-
+            <MenuGroup>
+                <div>Model S</div>
+                <div>Model X</div>
+                <div>Model Y</div>
+            </MenuGroup>
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+                <MenuIcon>
+                     
+                </MenuIcon>
+            </RightMenu>
         </Container>
     )
 }
@@ -19,5 +28,28 @@ export default Header;
 
 
 const Container = styled.div`
+    display: flex;
+    min-height: 60px;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+`
 
+const MenuGroup = styled.div`
+    display: flex;
+    text-transform: uppercase;
+    div {
+        font-weight: 600;
+        padding: 0 10px;
+        cursor: pointer;
+    }
+`
+
+const RightMenu = styled.div`
+    display: flex;
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding-right: 10px;
+    }
 `
