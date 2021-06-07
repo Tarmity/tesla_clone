f4fbdb0e6e69ae9de81d631f0a741e2301558f65
Tarmity Flassman <tarmity.flassman@gmail.com>
2021-06-07 15:53:24 +1000

    started coding the header and menu

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Container>
+            <a href="#"> 
+                <img src="/images/logo.svg" />
+            </a>
+        <MenuGroup>
+
+        </MenuGroup>
+
+        </Container>
+    )
+}
+
+export default Header;
+
+
+const Container = styled.div`
+
+`
