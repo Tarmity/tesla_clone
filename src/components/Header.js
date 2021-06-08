e56c00fb7811cc56d4f440833c79e27b91d8247d
Tarmity Flassman <tarmity.flassman@gmail.com>
2021-06-08 13:20:00 +1000

    added the sliding burger menu on the right hand side

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close'
+import { Translate } from '@material-ui/icons';
+
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+
+
     return (
         <Container>
             <a href="#">
@@ -19,10 +25,28 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Account</a>
-                <MenuIconContainer>
+                <MenuIconContainer onClick={()=>setBurgerStatus(true)}>
                      <MenuIcon />
                 </MenuIconContainer>
             </RightMenu>
+            <BurgerNav status={burgerStatus}>
+                <CloseContainer >
+                     <Close onClick={()=>setBurgerStatus(false)}/>
+                </CloseContainer>
+                
+                <li><a href="#">Model S</a></li>
+                <li><a href="#">Model Y</a></li>
+                <li><a href="#">Model X</a></li>
+                <li><a href="#">Model Y</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Invertory</a></li>
+                <li><a href="#">Trade-In</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Semi</a></li>
+                <li><a href="#">Charging</a></li>
+                
+            </BurgerNav>
         </Container>
     )
 }
@@ -40,6 +64,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 10;
 `
 
 const MenuGroup = styled.div`
@@ -49,6 +74,10 @@ const MenuGroup = styled.div`
         font-weight: 600;
         padding: 0 10px;
         cursor: pointer;
+    }
+
+    @media(max-width: 768px) {
+        display: none;
     }
 `
 
@@ -66,4 +95,33 @@ const MenuIconContainer = styled.div`
     display: flex;
     align-items: center;
     cursor: pointer;
+`
+const CloseContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+
+const Close = styled(CloseIcon)`
+    cursor: pointer;
+`
+
+const BurgerNav =styled.div`
+    position: fixed;
+    width: 300px;
+    background: white;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    padding: 20px;
+    list-style: none;
+    transform: ${props => props.status ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.2s ease-in;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+        a {
+           font-weight: 600;
+        }
+    }
+
 `
